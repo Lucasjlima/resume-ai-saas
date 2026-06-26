159de42aa17876edc8ff9efdc4557c5246bdf6f5
Lucas <lucasjlima2006@gmail.com>
2026-06-26 14:30:14 -0300

    fix: correct chatclient build and make aianalysis overall score deterministic

--- src/main/java/saas/com/br/resume_ai_saas/analyse/dto/response/AiAnalysisResult.java
+++ src/main/java/saas/com/br/resume_ai_saas/analyse/dto/response/AiAnalysisResult.java
@@ -1,3 +1,10 @@
 package saas.com.br.resume_ai_saas.analyse.dto.response;
 
-public record AiAnalysisResult(int overallScore, Feedback feedback) {}
+public record AiAnalysisResult(
+    int overallScore,
+    int hardSkillsScore,
+    int experienceScore,
+    int softSkillsScore,
+    int educationScore,
+    Feedback feedback
+) {}
